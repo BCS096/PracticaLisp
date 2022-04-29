dd8a27880792135ee7f8b008d2479bfb66120fce
BCS096 <56026175+BCS096@users.noreply.github.com>
2022-04-29 13:08:14 +0200

    fixed bugs

--- Practica1.lisp
+++ Practica1.lisp
@@ -3,6 +3,11 @@
             (t (cons (mapcar 'car l) (transposta (mapcar 'cdr l)))))
 )
 
+(defun mult (l)
+    (cond ((null l) 1)
+        (t (*(car l) (mult (cdr l)))))
+)
+
 (defun ProducteEscalar (l) 
     (apply '+ (mapcar 'mult (transposta l)))
 )
@@ -195,27 +200,25 @@
 ;(inicia-figura f): posa la figura f a la seva posició inicial (matriu identitat a la
 ;transformació)
 (defun inicia-figura (f)
-    (putprop f matriuIdentitat 'matriu)
+    (putprop f (matriuIdentitat) 'matriu)
 )
 
 ;--------------------------------------------------------------------
 ;COTI
 ;--------------------------------------------------------------------
-;No probado si funciona :)
-;--------------------------------------------------------------------
 
 (defun trasllada-figura (f x y z) ;'cub1 2 3 5
-    (putprop (multmatriu (get f 'matriu)(translacio x y z)) 'matriu)
+    (putprop f (multmatriu (get f 'matriu) (translacio x y z)) 'matriu)
 )
 
 (defun rota-figura (f x y z)
-    (putprop (multmatriu (get f 'matriu) (rotax x)) 'matriu)
-    (putprop (multmatriu (get f 'matriu) (rotay y)) 'matriu)
-    (putprop (multmatriu (get f 'matriu) (rotaz z)) 'matriu)
+    (putprop f (multmatriu (get f 'matriu) (rotax x)) 'matriu)
+    (putprop f (multmatriu (get f 'matriu) (rotay y)) 'matriu)
+    (putprop f (multmatriu (get f 'matriu) (rotaz z)) 'matriu)
 )
 
 (defun escala-figura (f x y z)
-    (putprop (multmatriu (get f 'matriu) (escalat x y z)) 'matriu)
+    (putprop f (multmatriu (get f 'matriu) (escalat x y z)) 'matriu)
 )
 ;--------------------------------------------------------------------
 ; DUDAS :
@@ -226,11 +229,52 @@
 ;--------------------------------------------------------------------
 ;MARC
 ;--------------------------------------------------------------------
-;Metodo que extrae lista de figuras de escena
-(defun pinta-figures ()
-    pinta-llista-figures ;(x llista)
+;[2:53 p. m., 28/4/2022] Coti: Te falta antes de pintar multiplicar todos los puntos por la matriz
+;de transformación de la figura i tmb sumar a cada punto no me acuerdo cuanto para que salga en medio de la pantalla
+;color
+(defun pinta-punts (a b) ;(0 0 0) , (1 0 0) Se ignora la z (3r parametro)
+    (move (car a) (cdr a));lapiz en 1r punto
+    (draw (car b) (cdr b));desplazamos lapiz al 2r punto pintando
+)
+
+(defun pinta-aresta-indv (a f) ;(1 2) , figura
+    pinta-punts( 
+        ;pos (-1- 2) de "punts ((0 0 0) (1 0 0) (1 0 1) (0 0 1) (0 1 0) (1 1 0) (1 1 1) (0 1 1))" luego (0 0 0)
+        (NTH (car a) (get (get f 'patro) 'punts)) 
+        ;pos (1 -2-) de "punts ((0 0 0) (1 0 0) (1 0 1) (0 0 1) (0 1 0) (1 1 0) (1 1 1) (0 1 1))" luego (1 0 0)
+        (NTH (cdr a) (get (get f 'patro) 'punts))
+    )
+)
+
+(defun pinta-arestas (a f) ;1 , figura
+    pinta-aresta-indv
+    (
+        ;pos -1- de "arestes ((1 2) (2 3) (3 4) (4 1) (1 5) (2 6) (3 7) (4 8) (5 6) (6 7) (7 8) (8 5))" luego (1 2)
+        (NTH (c) (get (get f 'patro) 'arestes))
+        f
+    )
+   ;concatena el resultado de llamar una vez a get-punts con car de lllista y otra vez a get-punts con cdr de llista luego con eso llama a pinta
+)
+(defun pinta-cara-indv (c f) ;(1 2 3 4), figura
+    (cond ((null c) nil)
+        (t (pinta-arestas ((car c)f) ;(-1- 2 3 4)
+           (pinta-cara-indv ((cdr c)f)) ;(1 -2- 3 4) 
+           )
+        )
+    )
+)
+(defun pinta-caras (c f) ;((1 2 3 4) (1 6 9 5) (2 6 10 7) (3 7 11 8) (4 8 12 5) (9 10 11 12)) , figura
+    (cond ((null c) nil)
+        (t (pinta-cara-indv ((car c)f) ;(1 2 3 4)
+           (pinta-caras (cdr c) f) ;... siguiente cara (x1,x2,x3,x4)
+           )
+        )
+    )
+)
+(defun pinta-figura (f) ;figura
+    pinta-caras 
         (
-            (get 'escena 'figures)
+            (get (get f 'patro) 'cares) ;cares ((1 2 3 4) (1 6 9 5) (2 6 10 7) (3 7 11 8) (4 8 12 5) (9 10 11 12))
         )
 )
 ;recorre lista x llamanda a pinta-figura para cada elem
@@ -245,61 +289,10 @@
         )
     )
 )
-
-;patro cub
-
-;punts ((0 0 0) (1 0 0) (1 0 1) (0 0 1) (0 1 0) (1 1 0) (1 1 1) (0 1 1))
-;arestes ((1 2) (2 3) (3 4) (4 1) (1 5) (2 6) (3 7) (4 8) (5 6) (6 7) (7 8) (8 5))
-;cares ((1 2 3 4) (1 6 9 5) (2 6 10 7) (3 7 11 8) (4 8 12 5) (9 10 11 12))
-
-(defun pinta-figura (f) ;figura
-    pinta-caras 
+;Metodo que extrae lista de figuras de escena
+(defun pinta-figures ()
+    pinta-llista-figures ;(x llista)
         (
-            (get (get f 'patro) 'cares) ;cares ((1 2 3 4) (1 6 9 5) (2 6 10 7) (3 7 11 8) (4 8 12 5) (9 10 11 12))
+            (get 'escena 'figures)
         )
-)
-
-
-(defun pinta-caras (c f) ;((1 2 3 4) (1 6 9 5) (2 6 10 7) (3 7 11 8) (4 8 12 5) (9 10 11 12)) , figura
-    (cond ((null c) nil)
-        (t (pinta-cara-indv ((car c)f) ;(1 2 3 4)
-           (pinta-caras (cdr c)) ;... siguiente cara (x1,x2,x3,x4)
-           )
-        )
-    )
-)
-
-(defun pinta-cara-indv (c f) ;(1 2 3 4), figura
-    (cond ((null c) nil)
-        (t (pinta-arestas ((car c)f) ;(-1- 2 3 4)
-           (pinta-cara-indv ((cdr c)f)) ;(1 -2- 3 4) 
-           )
-        )
-    )
-)
-
-(defun pinta-arestas (a f) ;1 , figura
-    pinta-aresta-indv
-    (
-        ;pos -1- de "arestes ((1 2) (2 3) (3 4) (4 1) (1 5) (2 6) (3 7) (4 8) (5 6) (6 7) (7 8) (8 5))" luego (1 2)
-        (NTH (c) (get (get f 'patro) 'arestes))
-        f
-    )
-   ;concatena el resultado de llamar una vez a get-punts con car de lllista y otra vez a get-punts con cdr de llista luego con eso llama a pinta
-)
-
-(defun pinta-aresta-indv (a f) ;(1 2) , figura
-    pinta-punts( 
-        ;pos (-1- 2) de "punts ((0 0 0) (1 0 0) (1 0 1) (0 0 1) (0 1 0) (1 1 0) (1 1 1) (0 1 1))" luego (0 0 0)
-        (NTH (car a) (get (get f 'patro) 'punts)) 
-        ;pos (1 -2-) de "punts ((0 0 0) (1 0 0) (1 0 1) (0 0 1) (0 1 0) (1 1 0) (1 1 1) (0 1 1))" luego (1 0 0)
-        (NTH (cdr a) (get (get f 'patro) 'punts))
-    )
-)
-;[2:53 p. m., 28/4/2022] Coti: Te falta antes de pintar multiplicar todos los puntos por la matriz
-;de transformación de la figura i tmb sumar a cada punto no me acuerdo cuanto para que salga en medio de la pantalla
-;color
-(defun pinta-punts (a b) ;(0 0 0) , (1 0 0) Se ignora la z (3r parametro)
-    (move (car a) (cdr a));lapiz en 1r punto
-    (draw (car b) (cdr b));desplazamos lapiz al 2r punto pintando
 )
