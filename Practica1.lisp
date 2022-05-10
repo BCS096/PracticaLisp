fac50704bb939417c258b40685b68a726f7c641a
Damián Gebhard Galeote <74540663+DGG568@users.noreply.github.com>
2022-05-10 13:17:35 +0200

    Comentado 1.0 ; Falta hacer el método NTH xd!!!
    Se puede comentar algo más, pero ramon dijo que mas o menos comentado que hacian los metodos y todo eso, no creo que quiera que comentamos que hacemos 3 car l y un cdr para multiplicar dos filas, no se se me he dejado alguna lo estaba haciendo en clase y me tengo que ir a casa xd

--- Practica1.lisp
+++ Practica1.lisp
@@ -1,16 +1,31 @@
+;-----------------------------------------
+; Pràctica LISP 
+; 21721 - Llenguatges de Programació. Grup 1 (Mallorca)
+; Bartomeu Capó Salas
+; Damián Gebhard Galeote
+; Marc Torres Torres
+; Emanuel Hegedüs
+;-----------------------------------------
+;-----------------------------------------
+; PART 1
+;-----------------------------------------
+;realitza la transposta d'una llista
 (defun transposta (l)
     (cond ((null (car l)) nil) 
             (t (cons (mapcar 'car l) (transposta (mapcar 'cdr l)))))
 )
 
+;multipplica cada element de la llista
 (defun mult (l)
     (cond ((null l) 1)
         (t (*(car l) (mult (cdr l)))))
 )
 
+;realitza el producte escalar 
 (defun ProducteEscalar (l) 
     (apply '+ (mapcar 'mult (transposta l)))
 )
+
 
 (defun unaFila (fila filas)
     (cond
@@ -26,18 +41,22 @@
     )
 )
 
+;multiplica dos matrius (llistes)
 (defun multMatriu (l1 l2)
     (operacio l1 (transposta l2))
 )
 
+;multiplica un punt per una matriu
 (defun multPunt (punt matriu)
     (unafila punt (transposta matriu))
 )
 
+;conversor de graus->radians
 (defun grausARadians (graus)
     (/ (* graus pi) 180)
 )
 
+;fica un elements al dinal d'una llista
 (defun snoc (x l) 
     (cond 
         ((null l) (cons x l))
@@ -45,10 +64,12 @@
     )
 )
 
+;translacio mitjançant 3 punts que després emprarem per a una figura
 (defun translacio (dx dy dz) 
     (list '(1 0 0 0) '(0 1 0 0) '(0 0 1 0) (snoc 1 (append (cons dx nil) (cons dy nil) (cons dz nil))))
 )
 
+;escalat mitjançant 3 punts que després emprarem per a una figura
 (defun escalat (ex ey ez)
     (list (append (cons ex nil) (cons 0 nil) (cons 0 nil) (cons 0 nil))
           (append (cons 0 nil) (cons ey nil) (cons 0 nil) (cons 0 nil))
@@ -57,6 +78,7 @@
     )
 )
 
+;rotar l'eix X  mitjançant un punt que després emprarem per a una figura
 (defun rotax (a)
     (list '(1 0 0 0)
           (append (cons 0 nil) (cons (cos (grausARadians a)) nil) (cons (- 0 (sin (grausARadians a))) nil) (cons 0 nil))
@@ -65,6 +87,7 @@
     )
 )
 
+;rotar l'eix Y  mitjançant un punt que després emprarem per a una figura
 (defun rotay (a)
     (list (append (cons (cos (grausARadians a)) nil) (cons 0 nil) (cons (- 0 (sin (grausARadians a))) nil) (cons 0 nil))
           '(0 1 0 0)
@@ -73,6 +96,7 @@
     )
 )
 
+;rotar l'eix Z  mitjançant un punt que després emprarem per a una figura
 (defun rotaz (a)
     (list (append (cons (cos (grausARadians a)) nil) (cons (- 0 (sin (grausARadians a))) nil) (cons 0 nil) (cons 0 nil))
           (append (cons (sin (grausARadians a)) nil) (cons (cos (grausARadians a)) nil) (cons 0 nil) (cons 0 nil))
@@ -81,9 +105,7 @@
     )
 )
 
-
-;patró cub
-
+;Inicialització del patró del cub
 (defun initCub ()
     (putprop 'cub '((-0.5 -0.5 -0.5) (0.5 -0.5 -0.5) (0.5 -0.5 0.5) (-0.5 -0.5 0.5) (-0.5 0.5 -0.5)
     (0.5 0.5 -0.5) (0.5 0.5 0.5) (-0.5 0.5 0.5)) 'punts)
@@ -93,8 +115,7 @@
     (4 8 12 5) (9 10 11 12)) 'cares)
 )
 
-;patró prisma
-
+;Inicialització del patró del prisma
 (defun initPrisma ()
     (putprop 'prisma '((-0.5 -0.5 0.5) (0.5 -0.5 0.5) (0 -0.5 -0.5) (-0.5 0.5 0.5) 
     (0.5 0.5 0.5) (0 0.5 -0.5)) 'punts)
@@ -104,8 +125,7 @@
     (3 6 9 4) (7 8 9)) 'cares)
 )
 
-;patró octaedre
-
+;Inicialització del patró del octaedre
 (defun initOctaedre ()
     (putprop 'octaedre '((-0.5 0 -0.5) (0.5 0 -0.5) (0.5 0 0.5) (-0.5 0 0.5) 
     (0 0.5 0) (0 -0.5 0)) 'punts)
@@ -115,16 +135,19 @@
     (4 8 5) (1 9 10) (2 10 11) (3 11 12) (4 12 9)) 'cares)
 )
 
+;inicialització de l'escena que contindrà la llista de figures
 (defun initEscena ()
     (putprop 'escena nil 'figures)          ;inicialmente vacia
 )
 
+;inicialitzam el patrons per tenir-los carregats
 (defun inicia-patrons ()
     (initCub)
     (initPrisma)
     (initOctaedre)
 )
 
+;borrar un element X d'una llista
 (defun borra-element (x llista)
     (cond ((null llista) nil)
         ((equal x (car llista)) (cdr llista))
@@ -132,34 +155,38 @@
     )
 )
 
+;borra la figura demanada de la llista de figures, i imprimeix una altre vegada les
+;figures però sense la borrada prèviament
 (defun borra-figura (f)
     (putprop 'escena (borra-element f (get 'escena 'figures)) 'figures)
     (cls)
     (pinta-figures)
 )
 
+;borra de la pantalla la figura demanada pero NO de la llsita de figures, sols de la pantalla
 (defun cls-figura (f)  
     (cls)
     (pinta-llista-figures (borra-element f (get 'escena 'figures)))
 )
 
+;borra totes les figures de la pantalla y de la llista de figures
 (defun borra-figures ()
     (putprop 'escena nil 'figures)
     (cls)
 )
-;------------------------------------------------------------------------------------------------------
 
+;patró de la matriu indentitat
 (defun  matriuIdentitat () 
     (list '(1 0 0 0) '(0 1 0 0) '(0 0 1 0) '(0 0 0 1))
 )
 
+;ficar una figura a una llista
 (defun ficarFigura (x l)
     (snoc x l)
 )
 
-;(crea-figura nom patró color): És una funció que permet la creació d’una figura 3D a partir
-;del patró triat i guarda aquesta figura dins la propietat “figures” de tipus llista d’un àtom
-;“escena”.
+;cream una figura dels 3 patrons que hi ha amb un nom y un color. També necessita tenir
+;la matriu identitat per defecte
 (defun crea-figura (n p c)
     (putprop n p 'patro)
     (putprop n c 'color)
@@ -167,55 +194,63 @@
     (putprop 'escena (ficarFigura n (get 'escena 'figures)) 'figures)
 )
 
-;(inicia-figura f): posa la figura f a la seva posició inicial (matriu identitat a la
-;transformació)
+;posa la figura f a la seva posició inicial (matriu identitat a la transformació)
 (defun inicia-figura (f)
     (putprop f (matriuIdentitat) 'matriu)
 )
 
+;mitjançant la multiplicació de matrius traslladam una figura
 (defun trasllada-figura (f x y z)
     (putprop f (multmatriu (get f 'matriu) (translacio x y z)) 'matriu)
 )
 
+;mitjançant la multiplicació de matrius rotam X/Y/Z una figura
 (defun rota-figura (f x y z)
     (putprop f (multmatriu (get f 'matriu) (rotax x)) 'matriu)
     (putprop f (multmatriu (get f 'matriu) (rotay y)) 'matriu)
     (putprop f (multmatriu (get f 'matriu) (rotaz z)) 'matriu)
 )
 
+;mitjançant la multiplicació de matrius escalam una figura
 (defun escala-figura (f x y z)
     (putprop f (multmatriu (get f 'matriu) (escalat x y z)) 'matriu)
 )
 
+;pinta dos punts d'una figura especifica
 (defun pinta-punts (a b f)
     (color (car (get f 'color)) (cadr (get f 'color)) (caddr (get f 'color)) )
-    ;               X                            Y
     (move (+ (realpart (round (car a))) 320) (+ (realpart (round (car (cdr a)))) 187));lapiz en 1r punto
     (draw (+ (realpart (round (car b))) 320) (+ (realpart (round (car (cdr b)))) 187));desplazamos lapiz al 2r punto pintando
 )
 
+;pinta una aresta d'una figura especifica
 (defun pinta-aresta-indv (a f) ;(1 2) , figura
     (pinta-punts 
-        (multpunt (snoc 1 (agafa-element (- (car a) 1) (get (get f 'patro) 'punts ) )) (get f 'matriu))
-        (multpunt (snoc 1 (agafa-element (- (car (cdr a)) 1) (get (get f 'patro) 'punts) )) (get f 'matriu))
+        (multpunt (snoc 1 (NTH (- (car a) 1) (get (get f 'patro) 'punts ) )) (get f 'matriu))
+        (multpunt (snoc 1 (NTH (- (car (cdr a)) 1) (get (get f 'patro) 'punts) )) (get f 'matriu))
          f
     )
 )
 
+;pinta arestes d'una figura especifica
 (defun pinta-arestas (a f) ;1 , figura
     (pinta-aresta-indv
         ;pos -1- de "arestes ((1 2) (2 3) (3 4) (4 1) (1 5) (2 6) (3 7) (4 8) (5 6) (6 7) (7 8) (8 5))" luego (1 2)
-        (agafa-element (- a 1) (get (get f 'patro) 'arestes))
+        (NTH (- a 1) (get (get f 'patro) 'arestes))
         f
     )
-   ;concatena el resultado de llamar una vez a get-punts con car de lllista y otra vez a get-punts con cdr de llista luego con eso llama a pinta
+   ;concatena el resultado de llamar una vez a get-punts con car de llista y otra vez a get-punts con cdr de llista luego con eso llama a pinta
 )
+
+;pinta una cara individual d'una figura
 (defun pinta-cara-indv (c f) ;(1 2 3 4), figura
     (cond ((null c) nil)
         (t (pinta-arestas (car c) f)  (pinta-cara-indv (cdr c) f) ;(1 -2- 3 4) 
         )
     )
 )
+
+;pinta les cares d'una figura
 (defun pinta-caras (c f) ;((1 2 3 4) (1 6 9 5) (2 6 10 7) (3 7 11 8) (4 8 12 5) (9 10 11 12)) , figura
     (cond ((null c) nil)
         (t (pinta-cara-indv (car c) f) ;(1 2 3 4)
@@ -223,6 +258,8 @@
         )
     )
 )
+
+;pinta una figura demanada
 (defun pinta-figura (f) ;figura
     (pinta-caras 
             (get (get f 'patro) 'cares) f ;cares ((1 2 3 4) (1 6 9 5) (2 6 10 7) (3 7 11 8) (4 8 12 5) (9 10 11 12))
@@ -240,6 +277,7 @@
     )
 )
 
+;NTH xd
 (defun agafa-element (pos llista)
     (cond ((null llista) nil)
           ((= pos 0) (car llista))
@@ -252,57 +290,68 @@
     (pinta-llista-figures (get 'escena 'figures))
 )
 ;-----------------------------------------
-; PARTE 2
+; PART 2
 ;-----------------------------------------
+;animació de la rotació d'una figura
 (defun anima-rotacio (f)
     (cls)
     (print 'rotacio)
     (pinta-figura f)
+    ;segons la tecla que pitjem pasarà diferents esdeveniments
     (setq key (get-key))
     (cond 
-        ((= key 336) (rota-figura f -5 0 0)(anima-rotacio f)) ;izq
-        ((= key 328) (rota-figura f 5 0 0)(anima-rotacio f)) ;der
-        ((= key 333) (rota-figura f 0 5 0)(anima-rotacio f)) ;up
-        ((= key 331) (rota-figura f 0 -5 0)(anima-rotacio f)) ;down
-        ((= key 113)) ;q
+        ((= key 336) (rota-figura f -5 0 0)(anima-rotacio f)) ;esquerra
+        ((= key 328) (rota-figura f 5 0 0)(anima-rotacio f)) ;dreta
+        ((= key 333) (rota-figura f 0 5 0)(anima-rotacio f)) ;adalt
+        ((= key 331) (rota-figura f 0 -5 0)(anima-rotacio f)) ;abaix
+        ((= key 113)) ;q (menú)
     )
 )
+
+;animació de la translacio d'una figura
 (defun anima-translacio (f)
     (cls)
     (print 'translacio)
     (pinta-figura f)
+    ;segons la tecla que pitjem pasarà diferents esdeveniments
     (setq key (get-key))
     (cond 
-        ((= key 331) (trasllada-figura f -5 0 0)(anima-translacio f)) ;izq
-        ((= key 333) (trasllada-figura f 5 0 0)(anima-translacio f)) ;der
-        ((= key 328) (trasllada-figura f 0 5 0)(anima-translacio f)) ;up
-        ((= key 336) (trasllada-figura f 0 -5 0)(anima-translacio f)) ;down
-        ((= key 113)) ;q
+        ((= key 331) (trasllada-figura f -5 0 0)(anima-translacio f)) ;esquerra
+        ((= key 333) (trasllada-figura f 5 0 0)(anima-translacio f)) ;dreta
+        ((= key 328) (trasllada-figura f 0 5 0)(anima-translacio f)) ;adalt
+        ((= key 336) (trasllada-figura f 0 -5 0)(anima-translacio f)) ;abaix
+        ((= key 113)) ;q (menú)
     )
 )
+
+;animació de l'escalat d'una figura
 (defun anima-escalat (f)
     (cls)
     (print 'escalat)
     (pinta-figura f)
+    ;segons la tecla que pitjem pasarà diferents esdeveniments
     (setq key (get-key))
     (cond 
-        ((= key 331) (escala-figura f (/ 1 2) 1 1)(anima-escalat f)) ;izq
-        ((= key 333) (escala-figura f 2 1 1)(anima-escalat f)) ;der
-        ((= key 328) (escala-figura f 1 2 1)(anima-escalat f)) ;up
-        ((= key 336) (escala-figura f 1 (/ 1 2) 1)(anima-escalat f)) ;down
-        ((= key 113)) ;q
+        ((= key 331) (escala-figura f (/ 1 2) 1 1)(anima-escalat f)) ;esquerra
+        ((= key 333) (escala-figura f 2 1 1)(anima-escalat f)) ;dreta
+        ((= key 328) (escala-figura f 1 2 1)(anima-escalat f)) ;adalt
+        ((= key 336) (escala-figura f 1 (/ 1 2) 1)(anima-escalat f)) ;abaix
+        ((= key 113)) ;q (menú)
     )
 )
+
+;animació total d'una figura
 (defun animacio (f)
     (cls)
     (print 'animacio)
     (pinta-figura f)
+    ;segons la tecla que pitjem pasarà diferents esdeveniments
     (setq key (get-key))
     (cond 
-        ((= key 114) (anima-rotacio f )(animacio f)) ;r
-        ((= key 116) (anima-translacio f )(animacio f)) ;t
-        ((= key 101) (anima-escalat f )(animacio f)) ;e
-        ((= key 113)) ;q
+        ((= key 114) (anima-rotacio f )(animacio f)) ;r (activa l'animacio de la rotació)
+        ((= key 116) (anima-translacio f )(animacio f)) ;t (activa l'animacio de la translació)
+        ((= key 101) (anima-escalat f )(animacio f)) ;e (activa l'animacio de l'escalat)
+        ((= key 113)) ;q (menu)
     )
     (cls)
 )
@@ -316,3 +365,4 @@
 (crea-figura 'cub1 'cub '(0 0 255))
 (escala-figura 'cub1 100 100 100)
 (cls)
+
